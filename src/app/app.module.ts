03756825e8a427fa2c522e393c3f7f76da26d670
Lakshya Arora <arora.lakshya123@gmail.com>
2020-10-24 16:10:57 +0530

    Revert "Add authentication module"
    This reverts commit 8f88c6524ef9018e8a77c0a95fd6460f4f56f4f5.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AuthenticationModule } from './authentication/authentication.module'
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AuthenticationModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
